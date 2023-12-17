94fab41e22f4a18436d27419bb2e54b13b2e8a2d
brunoamorim145 <brunoamorim145@gmail.com>
2023-12-17 17:29:42 -0300

    configuração das rotas concluida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { PortifolioComponent } from './pages/portifolio/portifolio.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path:'sobre',
+    component:SobreComponent
+  },
+  {
+    path:'habilidades',
+    component:HabilidadesComponent
+  },
+  {
+    path:'portfolio',
+    component:PortifolioComponent
+  },
+  {
+    path:'contato',
+    component:ContatoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
